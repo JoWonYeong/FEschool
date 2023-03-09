d547fc606d93008060eba2d3659e2750c19a5f7c
JoWonYeong <wswy17@gmail.com>
2023-03-10 00:06:50 +0900

    2023-03-09: js 35까지

--- JS100/035.js
+++ JS100/035.js
@@ -1,10 +1,10 @@
 // # 문제35 : Factory 함수 사용하기
 // 2제곱, 3제곱, 4제곱을 할 수 있는 Factory 함수를 만들려고 합니다.
-
 // <pass>에 코드를 작성하여 two함수를 완성하세요.
 
 function one(n) {
   function two(value) {
+    // <pass>
     // let result = value ** n;
     let result = Math.pow(value, n);
     return result;
